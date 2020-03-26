f1bb6bc2a5d84bf07bef0699b169b89e3777427b
TurBoss <j.l.toledano.l@gmail.com>
2020-03-26 22:32:45 +0100

    fix build

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+build_exe_options = {"packages": ["os"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
 
 setup(name="Terraform 3000",
       version="0.1",
